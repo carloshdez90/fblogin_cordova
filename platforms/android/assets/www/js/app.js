e0b1988fdabf582d1cbfb1b1b9cc5f2507d2af56
xscharlie <xscharliexs@gmail.com>
2014-09-23 13:11:02 -0600

    Login y navegacion entre 2 ventanas

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -3,20 +3,60 @@
   var module = angular.module('app', ['onsen']);
 
   module.controller('loginController', function($scope) {
+      /*Inicializacion de la variable comentario*/
+      
         ons.ready(function() {
           console.log('cargo');
+          openFB.init({appId: '874868115876369'});
         });
+      
+        /****************Control de login de facebook*****************/
         $scope.facebookLogin = function(){
             console.log('facebook');
-            toast.showShort('facebook');
-            $scope.Navigator.pushPage('page1.html', { animation : 'lift' } )
+            
+            openFB.login(
+                function(response) {
+                    if(response.status === 'connected') {
+                        //alert('Facebook login succeeded, got access token: ' + response.authResponse.token);
+                        /***************Mensaje de confirmacion y redireccion**************+*/
+                        $scope.Navigator.pushPage('page1.html', { animation : 'fade' } )
+                        toast.showShort('Te has logueado con facebook satisfactoriamente');
+                    } else {
+                        //alert('Facebook login failed: ' + response.error);
+                        toast.showShort('Facebook login failed: ' + response.error);
+                    }
+                }, {scope: 'email,read_stream,publish_stream'}
+            );
         }
         
+        /****************Control de login de twitter*****************/
         $scope.twitterLogin = function(){
             console.log('twitter');
             toast.showShort('twitter');
             $scope.Navigator.pushPage('page1.html', { animation : 'lift' } )
         }
+        
+        /****************Comentar en facebook *****************/
+        $scope.comentar = function(){
+            var elemento = document.querySelector( ".textarea");
+            
+            openFB.api({
+            method: 'POST',
+            path: '/me/feed',
+            params: {
+                message: elemento.value || 'Testing Facebook APIs'
+            },
+            success: function() {
+                toast.showShort('El mensaje fue posteado con exito');
+            },
+            error: function(error){
+                toast.showLong(error.message);
+            }
+            });
+            
+            //toast.showShort('Comentario exitoso');            
+        }
+
   });
 
 
